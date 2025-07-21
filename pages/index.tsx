27dfb5af9b2aaa8b6bae0e42278d0f55dd84f5b5
krak69 <clement.lacour@live.com>
2025-07-21 14:56:41 +0200

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Bienvenue sur Renfobase !</h1>
+      <p>Votre application de renforcement musculaire personnalisée démarre ici.</p>
+    </main>
+  );
+}
